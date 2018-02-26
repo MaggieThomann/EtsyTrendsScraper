07b903b1c422dbcf90caa5daa0b0be3080fcb3a2
MaggieThomann <mmthomann@gmail.com>
2018-02-26 18:06:24 -0500

    💯 Implemented refresh table view for trends!

--- EtsyTrendsScraper/DailyTrendsTableViewController.swift
+++ EtsyTrendsScraper/DailyTrendsTableViewController.swift
@@ -20,43 +20,15 @@ class DailyTrendsTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    
+        // Configure Refresh Control
+        self.refreshControl?.addTarget(self, action: #selector(DailyTrendsTableViewController.refreshTrends(_:)), for: UIControlEvents.valueChanged)
         
-        let step1 = OpenPageStep(
-            path: "https://www.etsy.com",
-            assertionName: "assertEtsyTitle")
-        let step2 = ScriptStep(
-        functionName: "checkTrends"){ response, _ in
-            
-            for trend in response as! [String]{
-                self.trendList.append(trend)
-            }
-            self.tableView.reloadData()
-            return .proceed
-        }
+        // Load the initial trends
+        self.retrieveTrends()
         
-        stepRunner = StepRunner(moduleName: "EtsyTrends", steps: [step1, step2])
-        stepRunner.insertWebViewIntoView(parent: view)
-        stepRunner.state.afterChange.add { change in
-            print("-----", change.newValue, "-----")
-            switch change.newValue {
-            case .inProgress(let index):
-                print("About to run step at index", index)
-            case .failure(let error):
-                print("Failed: ", error.localizedDescription)
-            case .success:
-                print("Finished successfully")
-            default:
-                break
-            }
-        }
-        stepRunner.run()
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -95,6 +67,49 @@ class DailyTrendsTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return CGFloat(50)
+    }
+    
+    // Refresh the trends
+    @objc func refreshTrends(_ sender: Any) {
+        self.retrieveTrends()
+        self.tableView.reloadData()
+        refreshControl?.endRefreshing()
+    }
+    
+    func retrieveTrends(){
+        // Javascript Stuff:
+        
+        self.trendList.removeAll()
+        
+        let step1 = OpenPageStep(
+            path: "https://www.etsy.com",
+            assertionName: "assertEtsyTitle")
+        let step2 = ScriptStep(
+        functionName: "checkTrends"){ response, _ in
+            
+            for trend in response as! [String]{
+                self.trendList.append(trend)
+            }
+            self.tableView.reloadData()
+            return .proceed
+        }
+        
+        stepRunner = StepRunner(moduleName: "EtsyTrends", steps: [step1, step2])
+        stepRunner.insertWebViewIntoView(parent: view)
+        stepRunner.state.afterChange.add { change in
+            print("-----", change.newValue, "-----")
+            switch change.newValue {
+            case .inProgress(let index):
+                print("About to run step at index", index)
+            case .failure(let error):
+                print("Failed: ", error.localizedDescription)
+            case .success:
+                print("Finished successfully")
+            default:
+                break
+            }
+        }
+        stepRunner.run()
     }
 
 }
